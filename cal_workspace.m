d667744253cbca413cea114f710469fc6394084c
liujimu <liujimu@outlook.com>
2017-03-12 20:39:01 +0800

    find boundary of the workspace

--- cal_workspace.m
+++ cal_workspace.m
@@ -1,14 +1,14 @@
 clear;
-% close all
+close all
 %%参数初始化
 qh_max=0.0435;
 qh_min=qh_max-0.13;
 qv_max=0.0375;
 qv_min=qv_max-0.13;
 dx=0.005;
-dr=0.005;
+dr=0.001;
 da=pi/180*5;
-r_max=0.1;
+r_max=0.12;
 a_max=pi/180*120;
 q_min=[qh_min*ones(1,3) qv_min*ones(1,3)];
 q_max=[qh_max*ones(1,3) qv_max*ones(1,3)];
@@ -18,40 +18,45 @@ ws=[];
 wsc=[];
 rm3=[];
 for x=qv_min:dx:qv_max
-    rm2=[];
     for a=0:da:a_max
         rm1=[];
-        for r=0:dr:r_max
-            pu=[x r*cos(a) r*sin(a)];
+        r=0:dr:r_max;
+        isWithinLimit=zeros(size(r));
+        for i=1:length(r)
+            pu=[x r(i)*cos(a) r(i)*sin(a)];
             angu=[0 0 0];
             [q,J]=InverseSolution_MOD(pu,[0 0 0]);
             in_limit=(q>=q_min)&(q<=q_max);
+            %判断是否超行程
             if(in_limit==ones(size(in_limit)))
-                ws=[ws;[pu r]];
-                wsc=[wsc;map_color(round(r/dr)+1,:)];
-                rm1=r;
+                isWithinLimit(i)=1;
             end
         end
-        rm2=[rm2 rm1];
+        %找边界
+        dif=diff(isWithinLimit);
+        for j=1:length(dif)
+            if(dif(j)==-1)
+                ws=[ws;[a,r(j),x]];
+            elseif(dif(j)==1)
+                ws=[ws;[a,r(j+1),x]];
+            else
+            end
+        end
     end
-    rm3=[rm3;[x min(rm2)]];
 end
 
 %%画图
 figure
-Rx=[1 0          0;
-    0 cos(a_max) -sin(a_max);
-    0 sin(a_max) cos(a_max)];
-ws1=ws(:,1:3)';
-ws2=Rx*ws1;
-ws3=Rx*ws2;
-ws_all=[ws1 ws2 ws3];
-wsc=[wsc;wsc;wsc];
-scatter3(ws_all(1,:),ws_all(2,:),ws_all(3,:),20,wsc,'.');
+ws_all=[ws;ws+repmat([pi/180*120 0 0],size(ws,1),1);ws+repmat([pi/180*240 0 0],size(ws,1),1)];
+
+[Y,Z,X] = pol2cart(ws_all(:,1),ws_all(:,2),ws_all(:,3));
+
+scatter3(X,Y,Z,'.');
 axis equal
 xlabel('x')
 ylabel('y')
 zlabel('z')
+
 
 %%计算工作空间内切圆柱
 % figure
