7dc6f6089fc8846e41a932f037395492b258e71d
liujimu <liujimu@outlook.com>
2017-12-17 10:43:00 +0800

    run PSO successfully

--- calibration_w_PSO_2.m
+++ calibration_w_PSO_2.m
@@ -1,3 +1,10 @@
 % PSO机构标定
 clear;
-param_errors = particleswarm(@fitness_calibration,60);
+global target_poses measured_poses
+target_poses = xlsread('target_poses.xlsx');
+measured_poses = xlsread('measured_poses.xlsx');
+nvars = 60;
+ub = [0.002*ones(1,6), 0.02*ones(1,18), 0.001*ones(1,18), 0.02*ones(1,18)];
+lb = -ub;
+options = optimoptions('particleswarm','Display','iter','SwarmSize',100,'HybridFcn',@fmincon);
+[param_errors,fval,exitflag,output] = particleswarm(@fitness_calibration,nvars,lb,ub,options);
--- fitness_calibration.m
+++ fitness_calibration.m
@@ -1,9 +1,10 @@
 function fitness_value = fitness_calibration( input )
 %fitness_calibration 用于标定的适应度函数
 %   此处显示详细说明
+    global target_poses measured_poses
     pkm = PKM();
-    target_poses = xlsread('target_poses.xlsx');
-    measured_poses = xlsread('measured_poses.xlsx');
+    %target_poses = xlsread('target_poses.xlsx');
+    %measured_poses = xlsread('measured_poses.xlsx');
     n = length(target_poses);
     real_qin = zeros(6,n);
     for i = 1:n
@@ -15,7 +16,11 @@ function fitness_value = fitness_calibration( input )
     calc_qin = zeros(6,n);
     for i = 1:n
         pkm_.pose = measured_poses(:,i);
-        calc_qin(:,i) = pkm_.q;
+        if isreal(pkm_.q)
+            calc_qin(:,i) = pkm_.q;
+        else
+            calc_qin(:,i) = ones(size(pkm_.q)).*inf;
+        end
     end
     dq = reshape(calc_qin - real_qin,[],1);
     fitness_value = norm(dq);
